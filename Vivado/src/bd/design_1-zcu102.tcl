6345d93949b76bb1326d1230e419170e0dc765e6
Jeff Johnson <jeff@fpgadeveloper.com>
2016-11-04 08:56:12 -0400

    updated for Vivado 2016.3 added lwIP modifications as a local SDK repo (EmbeddedSw directory)

--- Vivado/src/bd/design_1-zcu102.tcl
+++ Vivado/src/bd/design_1-zcu102.tcl
@@ -118,10 +118,12 @@ set_property -dict [list CONFIG.PRIM_SOURCE {Differential_clock_capable_pin} CON
 connect_bd_net [get_bd_pins clk_wiz_0/clk_out1] [get_bd_pins gmii_to_rgmii_2/clkin]
 
 # Connect ports for the Ethernet FMC 125MHz clock
-create_bd_port -dir I -from 0 -to 0 ref_clk_p
+create_bd_port -dir I -from 0 -to 0 -type clk ref_clk_p
 connect_bd_net [get_bd_pins /clk_wiz_0/clk_in1_p] [get_bd_ports ref_clk_p]
-create_bd_port -dir I -from 0 -to 0 ref_clk_n
+set_property CONFIG.FREQ_HZ 125000000 [get_bd_ports ref_clk_p]
+create_bd_port -dir I -from 0 -to 0 -type clk ref_clk_n
 connect_bd_net [get_bd_pins /clk_wiz_0/clk_in1_n] [get_bd_ports ref_clk_n]
+set_property CONFIG.FREQ_HZ 125000000 [get_bd_ports ref_clk_n]
 
 # Create IDELAYCTRL for the GMII-to-RGMII without shared logic
 create_bd_cell -type ip -vlnv xilinx.com:ip:util_idelay_ctrl util_idelay_ctrl_0
